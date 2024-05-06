c331a458aa89e8404570f23a586e25892d8fed41
Joel S. <nichtsylph@gmail.com>
2024-05-05 20:49:31 -0400

    added more changes on controller

--- server/src/main/java/server/oswegovrtserver/LocationController.java
+++ server/src/main/java/server/oswegovrtserver/LocationController.java
@@ -1,6 +1,7 @@
 package server.oswegovrtserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://moxie.cs.oswego.edu:26885")
 public class LocationController {
 
     @Autowired
--- server/src/main/java/server/oswegovrtserver/WebConfig.java
+++ server/src/main/java/server/oswegovrtserver/WebConfig.java
@@ -6,14 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @SuppressWarnings("null")
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
