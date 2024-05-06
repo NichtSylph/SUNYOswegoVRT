ffda29f0d010e7f75e2e26aec8a61f03f78e7d8f
Joel S. <nichtsylph@gmail.com>
2024-05-06 03:27:04 -0400

    XD

--- server/src/main/java/server/oswegovrtserver/WebConfig.java
+++ server/src/main/java/server/oswegovrtserver/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://moxie.cs.oswego.edu:26885"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
